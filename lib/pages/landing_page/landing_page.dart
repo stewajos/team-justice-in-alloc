cb4fdb8d4e9a1ca1c7fdad2e9a475000d236f119
kurpesk14 <kupresk@mail.gvsu.edu>
2020-09-21 18:12:26 -0400

    Update 1 - Landing Screen
    - Added all three needed text fields to the inital screen
    
    - Added styling and colors to the initial screen
    
    - Continue button is hooked up to the text controllers and prints out the data
    
    - Likely ready to be used as an initial app demonstration

--- lib/pages/landing_page/landing_page.dart
+++ lib/pages/landing_page/landing_page.dart
@@ -4,10 +4,14 @@ class LandingPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     TextEditingController emailController = new TextEditingController();
+    TextEditingController participantController = new TextEditingController();
+    TextEditingController supplyQuantController = new TextEditingController();
 
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        title: Text("Allocation App"),
+        backgroundColor: Colors.lightBlueAccent[200],
+        title: Text("Allocation App", style: TextStyle(color: Colors.black),),
       ),
       body: Center(
         child: Column(
@@ -15,14 +19,86 @@ class LandingPage extends StatelessWidget{
             Padding(
               padding: EdgeInsets.only(top: 20),
             ),
-            Text("Email", textAlign: TextAlign.left),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(padding: EdgeInsets.only(left: 20), child: Text("Email", style: TextStyle(fontSize: 15),),)
+              ],
+            ),
             Container(
               padding: EdgeInsets.all(20),
-              child:  TextField(
+              child:  TextFormField(
+                controller: emailController,
+                style: TextStyle(color: Colors.black),
                  decoration: InputDecoration(
-                   hintText: "Enter your email",
+                   fillColor: Colors.white,
+                   filled: true,
+                   hintText: "Enter Your Email",
                  ),
               )
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(padding: EdgeInsets.only(left: 20), child: Text("Applicant Count", style: TextStyle(fontSize: 15),),)
+              ],
+            ),
+            Container(
+                padding: EdgeInsets.all(20),
+                child:  TextFormField(
+                  controller: participantController,
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: "Enter number of participants",
+                  ),
+                )
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(padding: EdgeInsets.only(left: 20), child: Text("Supply Quantity", style: TextStyle(fontSize: 15),),)
+              ],
+            ),
+            Container(
+                padding: EdgeInsets.all(20),
+                child:  TextFormField(
+                  controller: supplyQuantController,
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: "Enter quantity of supplies",
+                  ),
+                )
+            ),
+            Padding(padding: EdgeInsets.only(top: 60)),
+            Container(
+              width: 1000,
+              padding: EdgeInsets.all(20),
+              child: FlatButton(
+                child: Text("Continue"),
+                color: Colors.lightBlueAccent[200],
+                onPressed: () {
+                    final userEmail = emailController.text;
+                    final participantNum = participantController.text;
+                    final supplyQuantity = supplyQuantController.text;
+
+                    //ToDo: Add testing for empty values
+                    ///Show dialog box when values are empty and do not continue
+
+                    print(userEmail);
+                    print(participantNum);
+                    print(supplyQuantity);
+                },
+              ),
             ),
           ],
         ),
