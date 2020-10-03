81fb236307e461410af9859541610dc916ee1a0e
kurpesk14 <kupresk@mail.gvsu.edu>
2020-10-03 18:13:37 -0400

    Added an email page
    - User can now type in their email to a text box. None of this is currently protected and is in a rough state. Just wanted to get it in place
    
    - Hitting next will bring the email over to the landing page and will display the landing page.
    
    - Some bugs with the side menu, ect.

--- lib/pages/landing_page/landing_page.dart
+++ lib/pages/landing_page/landing_page.dart
@@ -2,6 +2,10 @@ import 'package:allocation_app/pages/landing_page/widgets/navigation_drawer.dart
 import 'package:flutter/material.dart';
 
 class LandingPage extends StatelessWidget{
+  final String email;
+
+  LandingPage({@required this.email,});
+
   @override
   Widget build(BuildContext context) {
     // TextEditingController emailController = new TextEditingController();
@@ -14,12 +18,14 @@ class LandingPage extends StatelessWidget{
         elevation: 0.0,
         backgroundColor: Colors.blueAccent,
         centerTitle: true,
-        title: Text("Home", style: TextStyle(color: Colors.white)),
+        title: Text("Alek", style: TextStyle(color: Colors.white, fontSize: 20),),
       ),
       body: Stack(
         children: [
-          Container(color: Colors.grey), ///This is where the list of history will go!
-          NavigationDrawer(),
+          Container(color: Colors.grey), ///This is where the list of history will go / other aspects
+          NavigationDrawer(
+            email: email,
+          ),
         ],
       ),
     );
