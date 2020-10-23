30a2a346ad016dad479e30aebf6b09e1c5e6171e
SirBlooby <acrealabamawoo@gmail.com>
2020-10-23 01:05:49 -0400

    Sending some data to the db

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,15 +1,29 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 //we dont need this right now, and it's causing web deploy problems
 
-// class DatabaseService {
-//   // final String testingID; //in the future we are going to pass in this var in the Database Services instance
-//   // DatabaseService ({ this.testingID });
+class DatabaseService {
+  // final String testingID; //in the future we are going to pass in this var in the Database Services instance
+  // DatabaseService ({ this.testingID });
 
-//   // collection reference
-//   final CollectionReference testingCollection = Firestore.instance.collection('testing');
+  // collection reference
+  final CollectionReference testingCollection =
+      FirebaseFirestore.instance.collection('testing');
+  final CollectionReference databaseCollection =
+      FirebaseFirestore.instance.collection('database');
 
-//   Future updateCount() async {
-//     return await testingCollection.document("target").updateData({"count" : FieldValue.increment(1)});
-//   }
-// }
+  Future updateCount() async {
+    return await testingCollection
+        .doc("target")
+        .update({"count": FieldValue.increment(1)});
+  }
+
+  Future sendResult(recipients, supply, timestamp, selection) async {
+    return await databaseCollection.doc().set({
+      recipients: recipients,
+      supply: supply,
+      timestamp: timestamp,
+      selection: selection
+    });
+  }
+}
