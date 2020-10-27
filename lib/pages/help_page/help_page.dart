9efe7bb1df461a496834871d1faa8e533cfef4e9
Joshua Stewart <stewajos@mail.gvsu.edu>
2020-10-26 23:29:25 -0400

    still working on the Help page building containers but texts are there

--- lib/pages/help_page/help_page.dart
+++ lib/pages/help_page/help_page.dart
@@ -22,22 +22,29 @@ class HelpPage extends StatelessWidget{
      appBar: AppBar(
         title: Row(
           children: [
-            Text("Help", style: TextStyle(fontSize: 20 ))
+            Text("Help",
+                style: TextStyle(fontSize: 20 ),
+                textAlign: TextAlign.center)
           ],
      ),
       ),
-    body: Container(
+      body: Container(
+        padding: EdgeInsets.all(40),
         child: Column(
           children: [
             Text(
               "Patch Notes",
+          textAlign: TextAlign.center,
               style: TextStyle(
                   color: Colors.blueAccent,
                   fontSize: 20,
-                  fontWeight: FontWeight.bold),
+                  fontWeight: FontWeight.bold,
+              ),
             ),
+
             Text(
               "FAQ",
+              textAlign: TextAlign.center,
               style: TextStyle(
                   color: Colors.blueAccent,
                   fontSize: 20,
@@ -45,19 +52,9 @@ class HelpPage extends StatelessWidget{
             ),
           ],
         ),
-    ),
 
-    // body: Container(
-    //
-    // ),
-    //     color: Colors.white70,
-    //     child: Stack(
-    //       children: [
-    //         Text("During this time of Covid-19 spreading around the world, we have seen unprecedented needs for scarce medical resources. It has been found that the most ethical way to handle this dillema is to allocate resources in a completely random way. This would be done to avoid any biases / transgressions that may fuel a person to supply these resources to one party over the other despite being in the same situations. Our initial audience is the medical field but the application of this could be used for many related situations."
-    //          "")
-    //
-    //       ],
-    //     )
+      ),
+
      );
   }
 }
