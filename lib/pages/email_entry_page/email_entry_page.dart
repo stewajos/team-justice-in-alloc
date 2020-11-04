91cc97ddb7ffd3fde0cd7d9024df5a707ab8a8e7
SirBlooby <acrealabamawoo@gmail.com>
2020-11-04 17:59:28 -0500

    Added email validation

--- lib/pages/email_entry_page/email_entry_page.dart
+++ lib/pages/email_entry_page/email_entry_page.dart
@@ -3,7 +3,17 @@ import 'package:allocation_app/widgets/navigation_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class EmailEntryPage extends StatelessWidget {
+class EmailEntryPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return EmailEntryPageState();
+  }
+}
+
+class EmailEntryPageState extends State<EmailEntryPage> {
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     final allocationProvider = Provider.of<AllocationProvider>(context);
@@ -21,14 +31,27 @@ class EmailEntryPage extends StatelessWidget {
             Expanded(
                 child: Padding(
                   padding: EdgeInsets.only(left: 70, right: 70, top: 20),
-                  child: TextField(
-                    autofocus: false,
-                    controller: emailController,
-                    inputFormatters: [],
-                    decoration: InputDecoration(labelText: "Email", border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue, width: 3.0),
-                    )),
-                  ),
+                  child: Form(
+                    key: _formKey,
+                    child: TextFormField(
+                      autofocus: false,
+                      controller: emailController,
+                      validator: (value) {
+                        if(!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value)){
+                          return 'Invalid Email';
+                        }
+                      return null;
+                      },
+                      inputFormatters: [],
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue, width: 3.0),
+                        )
+                      ),
+                    )
+                  )
+
                 )
             )
           ],
@@ -45,18 +68,24 @@ class EmailEntryPage extends StatelessWidget {
               child: Text("BACK", style: TextStyle(color: Colors.white, fontSize: 15),),
             ),
             Padding(padding: EdgeInsets.only(left: 80, right: 80)),
-            FlatButton(onPressed:(){
-              allocationProvider.state.userEmail = emailController.text;
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) {
-                    return Scaffold(
-                        body: NavigationDrawer()
-                    );
-                  }));
-            },
-                padding: EdgeInsets.all(30),
-                child: Text("NEXT", style: TextStyle(color: Colors.white, fontSize: 15),)),
+            FlatButton(
+              onPressed:(){
+                if(_formKey.currentState.validate()) {
+                  allocationProvider.state.userEmail = emailController.text;
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) {
+                      return Scaffold(
+                          body: NavigationDrawer()
+                      );
+                    })
+                  );
+                }
+              },
+              padding: EdgeInsets.all(30),
+              child: Text("NEXT", style: TextStyle(color: Colors.white, fontSize: 15),
+              )
+            ),
           ],
         ),
       ),
