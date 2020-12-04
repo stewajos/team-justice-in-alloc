9801faafa630fbd9563aaa11feaeabe4d048964a
Kyler Kupres <kupresk@mail.gvsu.edu>
2020-12-03 22:46:23 -0500

    PDF Changes
    - Will now send the following things in the pdf:
    - User email
    - HashKey
    - Date
    - Number of applicants
    - Supply
    
    Working on list of selected applicants right now.

--- lib/services/email_service.dart
+++ lib/services/email_service.dart
@@ -1,6 +1,7 @@
 import 'package:allocation_app/model/result_model.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:mailgun/mailgun.dart';
+
 import 'dart:io';
 
 class EmailService {
--- lib/services/pdf_service.dart
+++ lib/services/pdf_service.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
-
+import 'package:allocation_app/model/recipient_model.dart';
 import 'package:allocation_app/model/result_model.dart';
+import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
@@ -19,16 +20,66 @@ class PdfService {
     return File('$path/result.pdf');
   }
 
-  Future<File> writeTestFile() async {
+  Future<File> writeTestFile(String userEmail, String hashKey, List<RecipientModel> recipients, int supply, int numApplicants, String timestamp) async {
     final file = await _localFile;
     final doc = pw.Document();
 
-    doc.addPage(
-      pw.Page(
-        build: (pw.Context context) => pw.Center(
-          child: pw.Text('Project Alek'),
+    doc.addPage(pw.MultiPage(
+     pageFormat: PdfPageFormat.letter.copyWith(marginBottom: 1.5 * PdfPageFormat.cm),
+      crossAxisAlignment: pw.CrossAxisAlignment.start,
+      header: (pw.Context context){
+       if(context.pageNumber == 1){
+         return null;
+       }
+       return pw.Container(
+         alignment: pw.Alignment.centerRight,
+         margin: const pw.EdgeInsets.only(bottom: 3.0 * PdfPageFormat.mm),
+         padding: const pw.EdgeInsets.only(bottom: 3.0 * PdfPageFormat.mm),
+         decoration: const pw.BoxDecoration(
+           border: pw.BoxBorder(
+             bottom: true, width: 0.5, color: PdfColors.grey50)),
+         child: pw.Text('Project Alek', style: pw.Theme.of(context).defaultTextStyle.copyWith(color: PdfColors.grey50)));
+      },
+      footer: (pw.Context context){
+       return pw.Container(
+         alignment: pw.Alignment.centerRight,
+         margin: const pw.EdgeInsets.only(top: 1.0 * PdfPageFormat.cm),
+         child: pw.Text(
+           "Page ${context.pageNumber} of ${context.pagesCount}",
+           style: pw.Theme.of(context).defaultTextStyle.copyWith(color: PdfColors.grey50)));
+      },
+      build: (pw.Context context) => <pw.Widget>[
+        pw.Header(
+          level: 0,
+          text: 'Project Alek - Allocation Report',
+          child: pw.Row(
+            mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+            children: <pw.Widget>[
+              pw.Text('Project Alek - Allocation Report', textScaleFactor: 2),
+              //Logo could go here for the PDF page
+            ]
+          )
         ),
-      ),
+        pw.Paragraph(
+          text: 'Here are the details of the allocation run: ',
+        ),
+        pw.Paragraph(
+          text: 'User Email: $userEmail \n'
+              'HashKey: $hashKey \n'
+              'Date Ran: $timestamp\n'
+              'Supply Quantity: $supply \n'
+              'Number of Applicants: $numApplicants\n',
+          style: pw.TextStyle(fontSize: 12)
+        ),
+        pw.Paragraph(
+          text: 'Applicants Selected',
+          style: pw.TextStyle(fontSize: 15, fontWeight: pw.FontWeight.bold),
+        ),
+        
+
+      ]
+
+    )
     );
 
     file.writeAsBytesSync(doc.save());
