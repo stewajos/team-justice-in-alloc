8ae1d49ac8153cae930d588eef7f8c0ce6092ef1
SirBlooby <acrealabamawoo@gmail.com>
2020-12-11 14:42:16 -0500

    updated email service to with new domain

--- lib/services/email_service.dart
+++ lib/services/email_service.dart
@@ -50,6 +50,42 @@ class EmailService {
     final RemoteConfig remoteConfig = await RemoteConfig.instance;
 
     final defaults = <String, dynamic>{
+      'mailgun_apikey': 'not a real apikey',
+      'mailgun_domain': 'not a real domain'
+    };
+    await remoteConfig.setDefaults(defaults);
+
+    await remoteConfig.fetch(expiration: const Duration(hours: 5));
+    await remoteConfig.activateFetched();
+
+    var domain = remoteConfig.getString('mailgun_domain');
+    var apikey = remoteConfig.getString('mailgun_apikey');
+
+    mailgun = MailgunMailer(
+        domain: 'mg.projalek.com', //TODO: Change this to mailgun_domain once the remote config updates
+        apiKey: apikey
+    );
+
+    var emailList = List<String>();
+    emailList.add(email);
+
+    //var file = new File('result.pdf');
+
+    var response = await mailgun.send(
+      from: "noreply@projalek.com",
+      to: emailList,
+      subject: "Project Alek Result",
+      text: "Attached is a pdf of your result. Thank you for using Project Alek. \n Sincerely, \n The Project Alek Team",
+      attachments: [pdf]
+    );
+
+    print(response.message);
+  }
+
+  Future<void> sendTestResultEmail(String email, File pdf) async {
+    final RemoteConfig remoteConfig = await RemoteConfig.instance;
+
+    final defaults = <String, dynamic>{
       'beta_mailgun_apikey': 'not a real apikey',
       'beta_mailgun_domain': 'not a real domain'
     };
@@ -67,19 +103,19 @@ class EmailService {
     );
 
     var to = List<String>();
-    to.add(email);
+    to.add('projalek@gmail.com');
 
     //var file = new File('result.pdf');
 
     var response = await mailgun.send(
-      from: "noreply@projalek.com",
-      to: to,
-      subject: "Project Alek Result",
-      text: "Attached is a pdf of your result. Thank you for using Project Alek. \n Sincerely, \n The Project Alek Team",
-      attachments: [pdf]
+        from: "noreply@projalek.com",
+        to: to,
+        subject: "Project Alek Result",
+        text: "Attached is a pdf of your result. Thank you for using Project Alek. \n Sincerely, \n The Project Alek Team",
+        attachments: [pdf]
     );
 
-    print("We sent the email?");
+    print("This is set to send to projalek@gmail.com by default");
     print(response.message);
   }
 }
